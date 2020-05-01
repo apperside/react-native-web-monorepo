15b0e4a126a90f78ecdab63e7be014672ff15a7e
Simone Gaspari <gaspari.simone@gmail.com>
2020-05-01 16:58:29 +0200

    checkpoint

--- apps/app/metro.config.js
+++ apps/app/metro.config.js
@@ -4,8 +4,29 @@
  *
  * @format
  */
+const path = require('path');
+const getWorkspaces = require('get-yarn-workspaces');
+const blacklist = require("metro-config/src/defaults/blacklist");
+const workspaces = getWorkspaces(__dirname);
+
+const watchFolders = [
+  path.resolve(__dirname, "..", "..", "node_modules"),
+  ...workspaces.filter((workspaceDir) => !(workspaceDir === __dirname))
+];
 
 module.exports = {
+  watchFolders,
+  blacklistRE: blacklist([
+    // Ignore other resolved react-native installations outside of
+    // myapp-native - this prevents a module naming collision when mapped.
+    /^((?!TaxyApp).)+[\/\\]node_modules[/\\]react-native[/\\].*/,
+
+    // Ignore react-native-svg dependency in myapp-ui, mapped below.
+    // react-native-svg must only be included once due to a side-effect. It
+    // has not been hoisted as it requires native module linking here.
+    // http://bit.ly/2LJ7V4b
+    // /myapp-ui[\/\\]node_modules[/\\]react-native-svg[/\\].*/,
+  ]),
   transformer: {
     getTransformOptions: async () => ({
       transform: {
---
+++ apps/app/src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import styled, { ThemeProvider } from "styled-components/native";
+import { App as AppContainer, Text } from "@apperside/app-ui/src";
+
+const Container = styled.View((props) => ({
+  backgroundColor: props.theme.colors.primary,
+  flex: 1
+}));
+
+export const App: React.FC = () => {
+  return (
+    <AppContainer>
+      <Grid flex={1}
+        backgroundColor="red"
+        width={1}>
+        <Row>
+          <Col width={1 / 2}>
+            <Text variant="H1">CIAO</Text>
+          </Col>
+          <Col width={1 / 2}>
+            <Text variant="H1">CIAO</Text>
+          </Col>
+        </Row>
+      </Grid>
+    </AppContainer>
+  );
+};
